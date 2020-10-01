94af0fcb1be572520a46f6edaaafdae6bc9b5e12
Kunal Yadav <52342747+kunalyadav684@users.noreply.github.com>
2020-10-01 09:55:22 +0530

    Update demoint.java

--- FOCP2/DataTypes/src/demoint.java
+++ FOCP2/DataTypes/src/demoint.java
@@ -3,7 +3,7 @@ class demoint
 {
 	public static void main(String [] args)
 	{
-		int a=10,b=20,add,sub,multi,div,mod;
+		int a=10,b=20,add,sub,multi,div,mod,base = 3, exponent = -4;
 		add=a+b;
 		sub=a-b;
 		multi=a*b;
@@ -14,5 +14,9 @@ class demoint
 		System.out.println("a*b="+multi);
 		System.out.println("b/a="+div);
 		System.out.println("b%a="+mod);
+	//to calculate the power of the function	
+        double result = Math.pow(base, exponent);
+
+        System.out.println("Answer = " + result);
 	}
 }
